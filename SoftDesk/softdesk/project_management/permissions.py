2f4b3e789b2bf6ebc2ef8031aa71cf10ad60ca54
Archy <antoine.tassez@gmail.com>
2023-08-07 15:54:59 +0200

    super_user now have author permissions

--- SoftDesk/softdesk/project_management/permissions.py
+++ SoftDesk/softdesk/project_management/permissions.py
@@ -6,4 +6,4 @@ class IsContributor(permissions.BasePermission):
 
 class IsAuthor(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.author == request.user
+        return obj.author == request.user or request.user.is_superuser
