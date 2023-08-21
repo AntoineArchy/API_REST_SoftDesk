7e839a84da38e958b91ee23664811abd8ed550d6
Archy <antoine.tassez@gmail.com>
2023-08-21 09:21:32 +0200

    Serializer now work mainly with validate() method

--- SoftDesk/softdesk/project_management/comment/serializer.py
+++ SoftDesk/softdesk/project_management/comment/serializer.py
@@ -7,43 +7,31 @@ from ..issue.models import Issue
 
 class CommentSerializer(serializers.HyperlinkedModelSerializer):
     parent_issue = serializers.HyperlinkedRelatedField(view_name='api:issue-detail', read_only=True)
+    uuid = serializers.HyperlinkedIdentityField(view_name="api:comment-detail", read_only=True)
     author = serializers.HyperlinkedRelatedField(view_name="api:user-detail", read_only=True)
-    uuid = serializers.HyperlinkedIdentityField(view_name="api:comment-detail")
 
     class Meta:
         model = Comment
-        fields = ['uuid', 'author', 'description', 'parent_issue']
-        extra_kwargs = {}
-
-    # def get_issue(self, obj):
-    #     request = self.context.get('request')
-    #     return request.build_absolute_uri()
+        fields = ['parent_issue', 'uuid', 'author', 'description', ]
 
     def validate(self, attrs):
         request = self.context.get('request')
-        user = request.user
-        issue_id = request.data.get('issue_id', False)
-        print(issue_id)
-        instance = getattr(self, 'instance', None)
+        if self.instance is None:
 
-        if instance is not None:
-            issue_id = instance.parent_issue.pk
+            parsed_url = parse_url(request.get_full_path())
+            issue_id = request.data.get('issue_id', parsed_url.get('issue_id', False))
+            if not issue_id:
+                raise serializers.ValidationError('Please provide a issue_id if not from url')
+            attrs['parent_issue'] = Issue.objects.get(pk=issue_id)
+            attrs['author'] = request.user
 
-        if not issue_id and not attrs.get('parent_issue', False):
-            url = request.get_full_path()
-            url_split = url.split('/')
-            try:
-                issue_id = url_split[5]
-
-            except IndexError as e:
-                raise serializers.ValidationError('If you\'r not creating from full url, '
-                                                  'please provide parent issue id')
-
-        if not attrs.get('parent_issue', False):
-
-            print('no p issue')
-            parent_issue = Issue.objects.get(pk=issue_id)
-            attrs['parent_issue'] = parent_issue
-
-        attrs['author'] = user
         return attrs
+
+def parse_url(url):
+    parsed_url = dict()
+    split_url = url.split('/')
+    for url_idx, url_part in enumerate(split_url):
+        if not url_part.isdigit():
+            continue
+        parsed_url[f"{split_url[url_idx - 1]}_id"] = url_part
+    return parsed_url
