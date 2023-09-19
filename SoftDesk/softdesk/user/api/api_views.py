c472049ea6a2c61af4ce8c25d169073d96b22ec4
Archy <antoine.tassez@gmail.com>
2023-09-19 09:30:35 +0200

    Added permission for user creation and custom destroy method to deactivate user instead of a real delete

--- SoftDesk/softdesk/user/api/api_views.py
+++ SoftDesk/softdesk/user/api/api_views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets, permissions
+from rest_framework.response import Response
 
+from project_management.project.models import Project
 from user.models import User
 from user.serializers import UserSerializer
+
+import project_management.permissions as pm_permissions
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -14,6 +18,22 @@ class UserViewSet(viewsets.ModelViewSet):
         permission_classes (list of classes): Classes de permission pour restreindre l'accès à la vue.
     """
 
-    queryset = User.objects.all()
+    queryset = User.objects.all().order_by('date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated, ]
+
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update', 'destroy']:
+            permission_classes = [pm_permissions.IsConcernedUser]
+        else:
+            permission_classes = [permissions.IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
+    def destroy(self, request, *args, **kwargs):
+        user = self.get_object()
+        user.is_active = False
+        user.save()
+        contribution_to_remove = Project.objects.filter(contributors=user)
+        for project in contribution_to_remove:
+            project.contributors.remove(user)
+        return Response(data='User has been deactivated.')
