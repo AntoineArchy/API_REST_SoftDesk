2d20d52ef2e8de26cf2d785a06a285fca01bf03d
Archy <antoine.tassez@gmail.com>
2023-09-19 09:31:12 +0200

    Added permission to know is the user doing an action is the user concerned by the action

--- SoftDesk/softdesk/project_management/permissions.py
+++ SoftDesk/softdesk/project_management/permissions.py
@@ -1,6 +1,27 @@
 from rest_framework import permissions
 
 
+class IsConcernedUser(permissions.BasePermission):
+    """
+    Classe de permission permettant de vérifier si l'utilisateur à l'origine de la requête est l'utilisateur
+    qu'on cherche à modifier.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Vérifie si l'utilisateur est un contributeur de la ressource.
+
+        Args:
+            request: L'objet de requête HTTP.
+            view: La vue associée à la requête.
+            obj: L'objet de la ressource sur lequel effectuer la vérification.
+
+        Returns:
+            bool: True si l'utilisateur est un contributeur, sinon False.
+        """
+        return request.user == obj or request.user.is_superuser
+
+
 class IsContributor(permissions.BasePermission):
     """
     Classe de permission permettant de vérifier si l'utilisateur est un contributeur d'une ressource.
@@ -28,7 +49,7 @@ class IsAuthor(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         """
-        Vérifie si l'utilisateur est l'auteur de la ressource ou un superutilisateur.
+        Vérifie si l'utilisateur est l'auteur de la ressource ou un super-utilisateur.
 
         Args:
             request: L'objet de requête HTTP.
@@ -36,6 +57,6 @@ class IsAuthor(permissions.BasePermission):
             obj: L'objet de la ressource sur lequel effectuer la vérification.
 
         Returns:
-            bool: True si l'utilisateur est l'auteur ou un superutilisateur, sinon False.
+            bool: True si l'utilisateur est l'auteur ou un super-utilisateur, sinon False.
         """
         return obj.author == request.user or request.user.is_superuser
